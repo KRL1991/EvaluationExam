8f9722caf530fc33838f766c4cec8553174914e6
KRL1991 <kimx4706@edu.easj.dk>
2020-12-22 13:30:37 +0100

    Added test - see comments to elaborate on erros - also added README

---
+++ out/production/EvaluationExam/sample/README.md
@@ -0,0 +1,3 @@
+Evaluation exam 22/12/2020  
+The program can show recipies  
+I had trouble with the arrays and therefor some of the later methods is missing (calculation of total calories and portions)
---
+++ src/sample/RecipesTest.java
@@ -0,0 +1,21 @@
+package sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipesTest {
+
+    // This is just to show that i can create a test - since i miss the correct values i cant test it for real
+    // The values are made up.
+    @org.junit.jupiter.api.Test
+    void TestcaloriesInTotal() {
+
+        assertEquals(100,100);
+    }
+
+    @org.junit.jupiter.api.Test
+    void TestcaloriesPrPortion() {
+
+        assertEquals(3,2);
+    }
+
+}
